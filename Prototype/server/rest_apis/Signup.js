4c2225839d7558534aeb5b47c9a959d5fbcf7c18
Bilal <mbilal.shahid10@gmail.com>
2022-11-15 18:17:44 +0500

    Backend Signup Tweaks

--- Prototype/server/rest_apis/Signup.js
+++ Prototype/server/rest_apis/Signup.js
@@ -5,7 +5,7 @@ const sanitize = require("mongo-sanitize")
 
 async function Signup(req, res){
     try{
-        if(res.role == 'Client'){
+        if(res.role == 'Influencer'){
             const role = sanitize (req.body.role)
             const fname = sanitize(req.body.name.f_name)
             const lname = sanitize(req.body.name.l_name)
@@ -13,10 +13,14 @@ async function Signup(req, res){
             const dob = sanitize(req.body.dob)
             const username = sanitize(req.body.username)
             const link = sanitize(req.body.link)
+            const niche = sanitize(req.body.niche)
             const password = sanitize(req.body.password)
-            await createClient(role ,fname , lname ,email , dob, username , link , password )
+            await createClient(role, fname, lname, email, dob, username, link, niche, password )
             res.status(200).json({
-                // msg: 'Client Signup successfully made!',
+                // msg: 'Influencer Signup successfully made!',
+                
+                // If createClient returns successfully, why are we sending the whole data of the entity again?
+                
                 role: role,
                 name: {
                     first: fname,
@@ -26,11 +30,12 @@ async function Signup(req, res){
                 dob: dob,
                 username: username,
                 profileLink: link,
+                niche: niche,
                 password: password
                 })
         }
 
-        else if(res.role == 'Influencer'){
+        else if(res.role == 'Client'){
             const role = sanitize (req.body.role)
             const name = sanitize(req.body.name)
             const category = sanitize(req.body.category)
@@ -40,9 +45,12 @@ async function Signup(req, res){
             const zipcode = sanitize(req.body.zipcode)
             const address =  sanitize(req.body.address)
             const password = sanitize(req.body.password)
-            await createInfluencer(role,name,category, email,startDate,country,zipcode,address, password )
+            await createInfluencer(role, name, category, email, startDate, country, zipcode, address, password )
             res.status(200).json({
-                // msg: 'Influencer Signup successfully made!',
+                // msg: 'Client Signup successfully made!',
+
+                // Same question as above in regards to sending this data back upon successfull signup
+                
                 role: role,
                 name: name,
                 category: category,
