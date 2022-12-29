67203cfac692dadc8d0543d4c659e99bf05b3c32
umairmohsin <23100233@lums.edu.pk>
2022-12-29 15:03:13 +0500

    Update App.js

--- Development/Sprint-1/client/src/App.js
+++ Development/Sprint-1/client/src/App.js
@@ -32,7 +32,7 @@ import RejectedJobOffer from "./Pages/Endorsee View/Orders/Rejected/RejectedJobO
 
 
 import RatingList from "./Pages/Organization View/Orders/Completed/RatingComp";
-import StarRating from "./Pages/Organization View/Orders/Completed/StarRating";
+// import StarRating from "./Pages/Organization View/Orders/Completed/StarRating";
 
 const App = ()=>{
   return(
